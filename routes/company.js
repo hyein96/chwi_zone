f8abe4c2902fd25a7e279eb0e4dcd95e8ca769f6
Jisu <jisupark0106@gmail.com>
2020-08-21 03:58:18 +0900

    server : company detail 구현
    stocks_id 에 따른 회사 상세 정보 가져오기

--- routes/company.js
+++ routes/company.js
@@ -9,12 +9,36 @@ var pool = mysql.createPool(config);
 
 
 router.get("/", function (req, res) {
+    //email 필요?  book mark stocks_id 
+    let stocks_id = req.query.stocks_id;
+    let email_address = req.query.email_address;
+    //let stocks_id = "4970";
+    //let email_address = "1";
+    console.log(stocks_id);
+  
+    //우선 email_address ="1"로 test
+    pool.getConnection(function (err, connection) {
+      //category list - > sector_id select
+      let get_company_query = `SELECT sector_name ,stocks_name FROM stock_code WHERE stocks_id =${stocks_id};`;
+      connection.query(get_company_query, function (err, rows) {
+        if (err) {
+          connection.release();
+          throw err;
+        } else {
+          if (rows.length == 0) {
+          } else {
+                   console.log(rows);
+                   ret = JSON.stringify(rows);
+                   result = JSON.parse(ret);
+                   console.log(result);
+                   res.json(result);
+                 }
+        }
+        connection.release();
+      });
+    });
+  });
 
-});
 
-
-router.post("/", function (req, res) {
-
-});
 
 module.exports = router;
