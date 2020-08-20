b0d29a5d773f9520e52a4df367b19e1e73c5dd10
Jisu <jisupark0106@gmail.com>
2020-08-20 21:26:57 +0900

    server : main 수정
    object 형식 수정

--- routes/main.js
+++ routes/main.js
@@ -7,8 +7,7 @@ var config = require("../db/db_info").local;
 var dbconfig = require("../db/db_con")();
 var pool = mysql.createPool(config);
 
-
-const company_list=[];
+const company_list = [];
 let company = new Object();
 
 router.get("/", function (req, res) {
@@ -18,12 +17,9 @@ router.get("/", function (req, res) {
   //우선 email_address ="1"로 test
   let email_address = "1";
   pool.getConnection(function (err, connection) {
-      //category list - > sector_id select
+    //category list - > sector_id select
     let get_sector_query = `SELECT sector_id FROM chwizone.category_code WHERE youtube_category IN(select youtube_category from chwizone.my_youtube where email_address="${email_address}" order by sub_count desc);`;
-    let get_sector = connection.query(get_sector_query, function (
-      err,
-      rows
-    ) {
+    let get_sector = connection.query(get_sector_query, function (err, rows) {
       if (err) {
         connection.release();
         throw err;
@@ -32,22 +28,22 @@ router.get("/", function (req, res) {
         } else {
           //console.log(rows);
           const delay = (ms) => {
-              //sector_id -> get company list
-          for (let i = 0; i < rows.length; i++) {
+            //sector_id -> get company list
+            for (let i = 0; i < rows.length; i++) {
               console.log(rows[i].sector_id);
               get_company(rows[i].sector_id);
-          }
-          return new Promise((resolve, reject) => {
-            try {
-              setTimeout(() => {
-                resolve();
-              }, ms);
-            } catch (err) {
-              reject(err);
             }
-          });
-        }
-        delay(1000).then(() => {
+            return new Promise((resolve, reject) => {
+              try {
+                setTimeout(() => {
+                  resolve();
+                }, ms);
+              } catch (err) {
+                reject(err);
+              }
+            });
+          };
+          delay(1000).then(() => {
             console.log(company_list);
             //send compnay_list array
             res.send(company_list);
@@ -57,38 +53,37 @@ router.get("/", function (req, res) {
 
       connection.release();
     });
-
   });
 });
 //1. get sector_name, 2.get stock_name, stock_id
-function get_company(sector_id){
-    let get_sector_name_query=`select distinct sector_name from chwizone.stock_code where sector_id = "${sector_id}";`;
-    let get_company_query=`select stocks_name,stocks_id from chwizone.stock_code where sector_id ="${sector_id}";`;
-    pool.getConnection(function (err, connection) {
-    let get_company_info = connection.query(get_sector_name_query+get_company_query, function (
-        err,
-        rows,fields
-      ) {
+function get_company(sector_id) {
+  let get_sector_name_query = `select distinct sector_name from chwizone.stock_code where sector_id = "${sector_id}";`;
+  let get_company_query = `select stocks_name,stocks_id from chwizone.stock_code where sector_id ="${sector_id}";`;
+  pool.getConnection(function (err, connection) {
+    let get_company_info = connection.query(
+      get_sector_name_query + get_company_query,
+      function (err, rows, fields) {
         if (err) {
           connection.release();
           throw err;
         } else {
           if (rows.length == 0) {
           } else {
-              //json으로 파싱... -> array 접근
+            //json으로 파싱... -> array 접근
             ret = JSON.stringify(rows);
             result = JSON.parse(ret);
-            company.sector_name = result[0][0];
+            company.sector_name = result[0][0].sector_name;
             company.company_list = result[1];
             console.log(company);
             //company_list compnay object push
             company_list.push(company);
-            company={};
+            company = {};
+          }
         }
-        }
-  
+
         connection.release();
-      });
-    });
-};
+      }
+    );
+  });
+}
 module.exports = router;
