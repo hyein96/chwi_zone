c653928b9b97defb230531bb4121483124c27355
Jisu <jisupark0106@gmail.com>
2020-08-22 15:11:15 +0900

    server : res.render 수정
    front-end 페이지 연결

--- routes/company.js
+++ routes/company.js
@@ -1,16 +1,20 @@
 const express = require('express');
 const router = express.Router();
 const request = require("request");
-
+​
 var mysql = require("mysql");
 var config = require("../db/db_info").local;
 var dbconfig = require("../db/db_con")();
 var pool = mysql.createPool(config);
-
-
-router.get("/", function (req, res) {
+​
+router.get("/", function(req,res){
+  res.render("company.ejs");
+});
+​
+router.get("/info", function (req, res) {
     //email 필요?  book mark stocks_id 
     let stocks_id = req.query.stocks_id;
+    console.log("main.ejs에서 받아온 값(query):"+req.query.stocks_id);
     let email_address = req.query.email_address;
     //let stocks_id = "4970";
     //let email_address = "1";
@@ -26,10 +30,11 @@ router.get("/", function (req, res) {
         } else {
           if (rows.length == 0) {
           } else {
-                   console.log(rows);
+                   //console.log(rows);
                    ret = JSON.stringify(rows);
                    result = JSON.parse(ret);
                    console.log(result);
+                   //company.ejs로 정보 보냄
                    res.json(result);
                  }
         }
@@ -37,7 +42,7 @@ router.get("/", function (req, res) {
       });
     });
   });
-
-
-
+​
+​
+​
 module.exports = router;
--- routes/youtube.js
+++ routes/youtube.js
@@ -1,19 +1,26 @@
 const express = require("express");
 const router = express.Router();
 const request = require("request");
+const path = require("path");
 var mysql = require("mysql");
 var config = require("../db/db_info").local;
 var dbconfig = require("../db/db_con")();
 var pool = mysql.createPool(config);
-
+​
+​
 //category 값 key, value 저장 -> db에 id와 함께 저장
 //id 가져오는거 방법 생각하기!!!!
 const my_youtube = [];
 let access_token;
+let email_address;
+​
 // channel 중복 값 거르기 위해 set 사용
-
+​
+//해당 이메일에 해당하는 token 받아서 youtube API 접근/데이터 받아옴
+//호출 후, 받아온 데이터 db에 저장 됨 
 router.get("/subscribe", function (req, res) {
-  let email_address = req.query.email_address;
+  email_address = req.query.email_address;
+  console.log("youtube.ejs에서 받아온 값:" + email_address); 
   pool.getConnection(function (err, connection) {
     //category list - > sector_id select
     let get_at_query = `SELECT access_token FROM user WHERE email_address="${email_address}";`;
@@ -41,52 +48,57 @@ router.get("/subscribe", function (req, res) {
               part: "snippet",
               mine: "true",
               maxResults: "50",
-              key: `apikey`,
+              //key: `apikey`,
               fields: "items(snippet(resourceId(channelId)))",
             },
           };
+          const delay = (ms) => {
+            request(option, function (error, response, body) {
+              if (error) {
+                console.error(error);
+                throw error;
+              } else {
+                var resultJson = JSON.parse(body);
+                let items = resultJson.items;
+                for (let i = 0; i < items.length; i++) {
+                  let item = items[i].snippet;
+                  let channelId = item.resourceId.channelId;
+                  //console.log(channelId);
+                  //token, channelId 넘겨서 channel 정보 가져오기
+                  getTopic(access_token, channelId);
+                }
+              }
+            });
+            return new Promise((resolve, reject) => {
+              try {
+                setTimeout(() => {
+                  resolve();
+                }, ms);
+              } catch (err) {
+                reject(err);
+              }
+            });
+          };
+        
+          delay(3000).then(() => {
+            count_value();
+            res.render("main.ejs",{ data: email_address });
+          });
+          //여기서(res 있는 곳) main.ejs로 바꾸며 email_address값 넘겨주기 
+          //email_address 전역으로 선언해서 DB insert 할 때 넣기(query로 받아온 것 까지 확인))
+         
         }
       }
       connection.release();
     });
   });
-
-  const delay = (ms) => {
-    request(option, function (error, response, body) {
-      if (error) {
-        console.error(error);
-        throw error;
-      } else {
-        var resultJson = JSON.parse(body);
-        let items = resultJson.items;
-        for (let i = 0; i < items.length; i++) {
-          let item = items[i].snippet;
-          let channelId = item.resourceId.channelId;
-          //console.log(channelId);
-          //token, channelId 넘겨서 channel 정보 가져오기
-          getTopic(access_token, channelId);
-        }
-      }
-    });
-    return new Promise((resolve, reject) => {
-      try {
-        setTimeout(() => {
-          resolve();
-        }, ms);
-      } catch (err) {
-        reject(err);
-      }
-    });
-  };
-
-  delay(3000).then(() => {
-    count_value();
-  });
-
+​
+  
+​
   // console.log(resultJson);
   //res.json(resultJson);
 });
-
+​
 function getTopic(access_token, channelId) {
   // for(let i=0;i<channels.length;i++){
   let option = {
@@ -99,7 +111,7 @@ function getTopic(access_token, channelId) {
     qs: {
       part: "topicDetails",
       id: channelId,
-      key: `apikey`,
+      //key: `apikey`,
       fields: "items(topicDetails)",
     },
   };
@@ -111,7 +123,7 @@ function getTopic(access_token, channelId) {
     get_category(topics);
   });
 }
-
+​
 function get_category(topics) {
   //console.log(topics)
   let str = "";
@@ -143,10 +155,10 @@ function get_category(topics) {
           }
         }
       }
-
+​
       connection.release();
     });
-
+​
     //console.log(my_youtube);
   });
 }
@@ -159,11 +171,11 @@ function count_value() {
     }
     return t;
   }, {});
-
+​
   console.log(result);
   for (let key in result) {
     pool.getConnection(function (err, connection) {
-      let insert_my_youtube_query = `INSERT INTO my_youtube (email_address, youtube_category, sub_count) VALUES('1', '${key}', ${result[key]});`;
+      let insert_my_youtube_query = `INSERT INTO my_youtube (email_address, youtube_category, sub_count) VALUES('${email_address}', '${key}', ${result[key]});`;
       let insert_my_youtube = connection.query(
         insert_my_youtube_query,
         function (err, rows) {
@@ -177,12 +189,11 @@ function count_value() {
               console.log("insert success");
             }
           }
-
+​
           connection.release();
-        }
-      );
+        });
     });
   }
 }
-
+​
 module.exports = router;
