bb74113d135e63356e984782098f6ea0aa96f547
Jisu <jisupark0106@gmail.com>
2020-08-24 11:12:06 +0900

    server : bookmark 구현
    bookmark 삽입/ 제거 구현

--- routes/bookmark.js
+++ routes/bookmark.js
@@ -1,4 +1,4 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 const request = require("request");
 
@@ -7,81 +7,64 @@ var config = require("../db/db_info").local;
 var dbconfig = require("../db/db_con")();
 var pool = mysql.createPool(config);
 
-router.get("/", function(req,res){
+router.get("/", function (req, res) {
   res.render("bookmark.ejs");
-})
+});
 router.get("/company", function (req, res) {
-   let email_address = req.query.email_address
-    //해당 email 구독하는 youtube_category 가져오기
-    //category랑 매칭하는 sector_id 가져오기
-  
-    //우선 email_address ="1"로 test
-    //let email_address = "1";
-    pool.getConnection(function (err, connection) {
-      //category list - > sector_id select
-      let get_bookmark_query = `SELECT stocks_id,stocks_name FROM chwizone.stock_code WHERE stocks_id IN(select stocks_id from chwizone.bookmark where email_address="${email_address}");`;
-      connection.query(get_bookmark_query, function (err, rows) {
-        if (err) {
-          connection.release();
-          throw err;
-        } else {
-          if (rows.length == 0) {
-            res.json("bookmark 정보 없음")
-          } else {
-                   console.log(rows);
-                   ret = JSON.stringify(rows);
-                   result = JSON.parse(ret);
-                   console.log(result);
-                   res.json(result);
-                 }
-        }
-        connection.release();
-      });
-    });
-  });
-
-// 수정중 수정중 수정중
-// 수정중 수정중 수정중
-// 수정중 수정중 수정중
-// 수정중 수정중 수정중
-// 수정중 수정중 수정중
-// 수정중 수정중 수정중
-router.post("/", function (req, res) {
-  //let email_address = req.query.email
-  //let email_address = req.query.stocks_id
-
-  let email_address = "1";
-  let stocks_id ="6040"
+  let email_address = req.query.email_address;
+  //email_address -> stocks_id, stocks_name 설정
   pool.getConnection(function (err, connection) {
-    //category list - > sector_id select
-    let find_bookmark_query = `select * from bookmark where email_address="${email_address}" and stocks_id ="${stocks_id}";`;
-    let find_bookmark = connection.query(find_bookmark_query, function (err, rows) {
+    let get_bookmark_query = `SELECT stocks_id,stocks_name FROM chwizone.stock_code WHERE stocks_id IN(select stocks_id from chwizone.bookmark where email_address="${email_address}");`;
+    connection.query(get_bookmark_query, function (err, rows) {
       if (err) {
         connection.release();
         throw err;
       } else {
         if (rows.length == 0) {
-          let insert_bm = `insert into bookmark (email_address, stocks_id) values ("${email_address}","${stocks_id}");`;
-              connection.query(insert_bm, function (err, results) {
-                if (err) throw err;
-                else {
-                  res.json("bookmark deleted..");
-                }
-              });
+          res.json("bookmark 정보 없음");
         } else {
-          let delete_bm = `delete from bookmark where email_address="${email_address}" and stocks_id="${stocks_id}";`;
-          connection.query(delete_bm, function (err, results) {
-          if (err) throw err;
-          else {
-            res.json("bookmark update..");
-          }
-        });
-               }
+          //bookmark 정보 있으면 출력
+          console.log(rows);
+          ret = JSON.stringify(rows);
+          result = JSON.parse(ret);
+          console.log(result);
+          res.json(result);
+        }
       }
-    
       connection.release();
     });
   });
+});
+
+router.post("/", function (req, res) {
+  console.log(req.body);
+  let email_address = req.body.email_address;
+  let stocks_id = req.body.stocks_id;
+  let order = req.body.order;
+  //order - insert : bookmark 삽입 , delete : bookmark 삭제
+  if (order == "insert") {
+    pool.getConnection(function (err, connection) {
+      let insert_bm = `insert into bookmark (email_address, stocks_id) values ("${email_address}","${stocks_id}");`;
+      connection.query(insert_bm, function (err, results) {
+        if (err) throw err;
+        else {
+          res.json("bookmark 입력");
+        }
+        connection.release();
+      });
+    });
+  } else if (order == "delete") { 
+    pool.getConnection(function (err, connection) {
+    let delete_bm = `delete from bookmark where email_address="${email_address}" and stocks_id="${stocks_id}";`;
+    connection.query(delete_bm, function (err, results) {
+      if (err) throw err;
+      else {
+        res.json("bookmark 제거");
+      }
+      connection.release();
+    });
+  });
+  }
 });
 
 module.exports = router;
