8dfb3507a04c4b82906e1e87666457703326afd1
hyein96 <42136782+hyein96@users.noreply.github.com>
2020-08-24 23:03:15 +0900

    commit

--- routes/company.js
+++ routes/company.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const request = require("request");
+const axios = require("axios");
+const cheerio = require("cheerio");
 
 let mysql = require("mysql");
 let config = require("../db/db_info").local;
@@ -42,6 +44,52 @@ router.get("/info", function (req, res) {
       });
     });
   });
+
+  //회사가격,차트 router
+let priceChart = new Array();
+let stocks_id;
+router.get('/price', function (req, res) {
+  stocks_id = req.query.stocks_id;
+  //네이버증권 주식코드: 6자리(DB에서 가져온 코드 6자리이하면 앞에 0추가)
+  while(stocks_id.length != 6){
+      stocks_id = "0"+stocks_id;
+  }
+  console.log(stocks_id);
+  //stocks_id = '097950';
+  getData(function () {
+    console.log(priceChart);
+    res.json(priceChart);
+  });
+  priceChart = new Array();
+
+});
+
+function getData(callback) {
+  var companyPrice;
+  var companyChart;
+  scrapingResult = {
+    'price': ''
+  }
+  
+  request(`https://finance.naver.com/item/main.nhn?code=${stocks_id}`, function (err, res, body) {
+    const $ = cheerio.load(body);
+    const bodyList = $(".today").map(function (i, element) {
+      scrapingResult['price'] = String($(element).find('p:nth-of-type(1)').text());
+      //숫자 뺀 모든 기호를 patter 변수에 넣기
+      var pattern = /[^(0-9)]/gi;
+      if (pattern.test(scrapingResult['price'])) {
+        companyPrice = scrapingResult['price'].replace(pattern, "");
+      }
+      companyPrice = companyPrice.substring(0, (companyPrice.length / 2));
+      //console.log(companyPrice); 
+      priceChart.push(companyPrice);
+    });
+    companyChart = $("#img_chart_area").attr('src'); //차트 이미지
+    //console.log(companyChart);
+    priceChart.push(companyChart);
+  });
+  setTimeout(callback, 1000);
+}
 
 
 
