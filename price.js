4bfe93eacdeaa307942b228d55779200d888eb40
sangwoojung <42603915+sangwoojung@users.noreply.github.com>
2020-08-24 17:59:00 +0900

    Add files via upload

---
+++ price.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const router = express.Router();
+const request = require('request');
+const cheerio = require('cheerio');
+
+scrapingResult = {
+    'price': ''
+}
+
+function getData(){
+    request("https://finance.naver.com/item/main.nhn?code=005930", function (err, res, body){
+        const $ = cheerio.load(body);
+        const bodyList = $(".today").map(function(i,element){
+            scrapingResult['price'] = String($(element).find('p:nth-of-type(1)').text());
+
+            console.log(scrapingResult)
+        });
+    });
+}
+
+getData();
