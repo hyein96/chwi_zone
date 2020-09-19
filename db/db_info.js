eeceb4a6480cc185d54bb9cadd14d3aff631bda5
Jisu <jisupark0106@gmail.com>
2020-09-20 00:02:46 +0900

    cloud : db 접근

--- db/db_info.js
+++ db/db_info.js
@@ -2,9 +2,9 @@ module.exports = (function () {
     return {
       local: {
         // localhost
-        host: "localhost",
-        user: "root",
-        password: "park7645",
+        host: "db-4s8a3.pub-cdb.ntruss.com",
+        user: "chwizone",
+        password: "chwizone1234^^",
         database: "chwizone",
         port: "3306",
         multipleStatements: true 
