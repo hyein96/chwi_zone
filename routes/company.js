ccec1b2d27b1787c07da96b835da7d437b217588
rlagwns1996 <58473356+rlagwns1996@users.noreply.github.com>
2020-09-19 17:35:11 +0900

    UPDATE: newsList 기능

--- routes/company.js
+++ routes/company.js
@@ -43,7 +43,7 @@ router.get("/info", function (req, res) {
         connection.release();
       });
     });
-  });
+});
 
   //회사가격,차트 router
 let priceChart = new Array();
@@ -61,7 +61,6 @@ router.get('/price', function (req, res) {
     res.json(priceChart);
   });
   priceChart = new Array();
-
 });
 
 function getData(callback) {
@@ -75,7 +74,7 @@ function getData(callback) {
     const $ = cheerio.load(body);
     const bodyList = $(".today").map(function (i, element) {
       scrapingResult['price'] = String($(element).find('p:nth-of-type(1)').text());
-      //숫자 뺀 모든 기호를 patter 변수에 넣기
+      //숫자 뺀 모든 기호를 pattern 변수에 넣기
       var pattern = /[^(0-9)]/gi;
       if (pattern.test(scrapingResult['price'])) {
         companyPrice = scrapingResult['price'].replace(pattern, "");
@@ -90,6 +89,43 @@ function getData(callback) {
   });
   setTimeout(callback, 1000);
 }
+
+router.get('/news', function(req, res) {
+  let companyName = req.query.company_name;
+  let newsNum = 6; //뉴스 갯수
+  let news = Array.from(Array(newsNum), () => new Array()); //2차원배열([뉴스갯수][]) 선언 => 제목,링크 2개는 밑에서 초기화
+  console.log("------" +companyName);
+  let option = {
+    method: "GET",
+    url: "https://openapi.naver.com/v1/search/news.json",
+    headers: {
+      "X-Naver-Client-Id": "ySqRfeHBGnhjTRtd96pZ",
+      "X-Naver-Client-Secret": "JDq_RtRsKb",
+    },
+    //form 형태는 form / 쿼리스트링 형태는 qs / json 형태는 json ***
+    qs: {
+      query: companyName,
+      sort: "sim",
+      display: newsNum,
+    },
+  };
+  request(option, function (error, response, body) {
+    let resultJson = JSON.parse(body);
+    console.log(resultJson);
+    for(var i = 0 ; i < newsNum ; i++){
+      var bTag = /[<b>;/]/gi;
+      var newsTopic = (resultJson.items[i].title).replace(bTag,"");
+      newsTopic = newsTopic.replace("&amp",'&');
+      newsTopic = newsTopic.replace("&quot",'"');
+      var newsLink = resultJson.items[i].link;
+      news[i].push(newsTopic);
+      news[i].push(newsLink);
+    }
+    console.log(news);
+    res.json(news);
+  });
+});
+
 
 
 
